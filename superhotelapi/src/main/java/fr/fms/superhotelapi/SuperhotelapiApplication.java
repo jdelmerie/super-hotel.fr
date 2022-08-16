01a07130e108cebec5f0b3967b0e309a4d49602c
Delmerie <j.delmerie@live.fr>
2022-08-16 11:07:04 +0200

    create spring api

---
+++ superhotelapi/src/main/java/fr/fms/superhotelapi/SuperhotelapiApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.superhotelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperhotelapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperhotelapiApplication.class, args);
+	}
+
+}
---
+++ superhotelapi/src/test/java/fr/fms/superhotelapi/SuperhotelapiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.superhotelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperhotelapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
